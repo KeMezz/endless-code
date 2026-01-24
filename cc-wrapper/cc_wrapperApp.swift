51469dae6bff406fdf8ff7e0418e52453a1f101f
jin <pnescene@icloud.com>
2026-01-25 01:24:23 +0900

    Initial Commit

---
+++ cc-wrapper/cc_wrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  cc_wrapperApp.swift
+//  cc-wrapper
+//
+//  Created by hyeongjin on 2026/01/25.
+//
+
+import SwiftUI
+
+@main
+struct cc_wrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cc-wrapperTests/cc_wrapperTests.swift
@@ -0,0 +1,16 @@
+//
+//  cc_wrapperTests.swift
+//  cc-wrapperTests
+//
+//  Created by hyeongjin on 2026/01/25.
+//
+
+import Testing
+
+struct cc_wrapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
